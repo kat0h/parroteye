6c5e89978af86ba138672b8189bf3e598c45b546
Kota Kato <peony.btn@gmail.com>
2021-02-16 23:30:55 +0900

    update

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import math
 import PySimpleGUI as sg
 import pyautogui
 
@@ -7,23 +8,45 @@ def get_cursorpos():
 
 
 def get_winpos(window):
-    return window.CurrentLocation()
+    pos = window.CurrentLocation()
+    ret = []
+    ret.append(pos[0] + window.Size[0] // 2)
+    ret.append(pos[1] + window.Size[1] // 2)
+    return ret
+
+
+def calc_angle(x, y, x2, y2):
+    angle = math.degrees(-math.atan2(y2-y, x2-x))
+    return angle if angle > 0 else angle + 360
 
 
 sg.theme('Default1')
 
 layout = [
-        [sg.T(size=(20, 1), justification='center', key='-mousepos-')],
-        [sg.T(size=(20, 1), justification='center', key='-winpos-')],
+        [sg.Image(key='-image-')],
         ]
 
-window = sg.Window('Watch', layout)
+window = sg.Window('Parroteye', layout)
 
+images = [
+        './img/0.gif',
+        './img/1.gif',
+        './img/2.gif',
+        './img/3.gif',
+        './img/4.gif',
+        './img/5.gif',
+        './img/6.gif',
+        './img/7.gif',
+        './img/8.gif',
+        './img/9.gif',
+        ]
 while True:
     event, values = window.read(timeout=1000/60, timeout_key='-timeout-')
 
     if event in (None,):
         break
     elif event in '-timeout-':
-        window['-mousepos-'].update(get_cursorpos())
-        window['-winpos-'].update(get_winpos(window))
+        cursorpos = get_cursorpos()
+        winpos = get_winpos(window)
+        angle = calc_angle(winpos[0], winpos[1], cursorpos[0], cursorpos[1])
+        window['-image-'].update(images[int(angle//36)])
